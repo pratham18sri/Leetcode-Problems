c742cd326d2060499c9b641157125726e872bb45
PRATHAM SRIVASTAV <139158707+pratham18sri@users.noreply.github.com>
2026-03-17 11:23:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (94.15%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -5,7 +5,8 @@ class Solution {
         return 1 + Math.max(height(root.left), height(root.right));
     }
 
-    public static void nthlevel(TreeNode root, int n, List<Integer> list, boolean leftToRight){
+    // LEFT → RIGHT
+    public static void leftToRight(TreeNode root, int n, List<Integer> list){
         if(root == null) return;
 
         if(n == 1){
@@ -13,26 +14,37 @@ class Solution {
             return;
         }
 
-        if(leftToRight){
-            nthlevel(root.left, n-1, list, leftToRight);
-            nthlevel(root.right, n-1, list, leftToRight);
-        } else {
-            nthlevel(root.right, n-1, list, leftToRight);
-            nthlevel(root.left, n-1, list, leftToRight);
+        leftToRight(root.left, n-1, list);
+        leftToRight(root.right, n-1, list);
+    }
+
+    // RIGHT → LEFT
+    public static void rightToLeft(TreeNode root, int n, List<Integer> list){
+        if(root == null) return;
+
+        if(n == 1){
+            list.add(root.val);
+            return;
         }
+
+        rightToLeft(root.right, n-1, list);
+        rightToLeft(root.left, n-1, list);
     }
 
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         int h = height(root);
 
-        boolean leftToRight = true;
-
         for(int i = 1; i <= h; i++){
             List<Integer> level = new ArrayList<>();
-            nthlevel(root, i, level, leftToRight);
+
+            if(i % 2 == 0){
+                rightToLeft(root, i, level); // even → RTL
+            } else {
+                leftToRight(root, i, level); // odd → LTR
+            }
+
             ans.add(level);
-            leftToRight = !leftToRight; // flip direction
         }
 
         return ans;
