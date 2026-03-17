5dfa25d8cb23f0677446c81da138ee2a5a999d72
PRATHAM SRIVASTAV <139158707+pratham18sri@users.noreply.github.com>
2026-03-17 11:20:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (57.18%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -1,42 +1,40 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> arr = new ArrayList<>();
-        if (root == null) return arr;
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        boolean isForward=true;
-        while (!q.isEmpty()) {
-            int size = q.size(); // number of nodes at the current level
-            List<Integer> arr1 = new ArrayList<>();
+    public static int height(TreeNode root){
+        if(root == null) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
 
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                if( isForward) arr1.addLast(node.val);
-                else arr1.addFirst(node.val);
-                if (node.left != null) q.add(node.left);
-                if (node.right != null) q.add(node.right);
-            }
+    public static void nthlevel(TreeNode root, int n, List<Integer> list, boolean leftToRight){
+        if(root == null) return;
 
-            arr.add(arr1);
-            isForward= !isForward;
+        if(n == 1){
+            list.add(root.val);
+            return;
         }
 
-        return arr;
+        if(leftToRight){
+            nthlevel(root.left, n-1, list, leftToRight);
+            nthlevel(root.right, n-1, list, leftToRight);
+        } else {
+            nthlevel(root.right, n-1, list, leftToRight);
+            nthlevel(root.left, n-1, list, leftToRight);
+        }
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int h = height(root);
+
+        boolean leftToRight = true;
+
+        for(int i = 1; i <= h; i++){
+            List<Integer> level = new ArrayList<>();
+            nthlevel(root, i, level, leftToRight);
+            ans.add(level);
+            leftToRight = !leftToRight; // flip direction
+        }
+
+        return ans;
     }
 }
