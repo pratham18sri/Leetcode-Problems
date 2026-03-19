7370896b7ac2c2eeaf63eff5ac0e4b70ee24421e
PRATHAM SRIVASTAV <139158707+pratham18sri@users.noreply.github.com>
2026-03-19 11:59:09 +0530

    Time: 3 ms (97.77%) | Memory: 52 MB (84.76%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,25 +1,22 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int starting_k=k;
-        int i=0;
-        int j=0;
-        int maxlength=-1;
-        while(j<nums.length){
-            // ager 0 hai to k ek kam hota jayega 
-            if( nums[j]==0){
-                k--;
+        //applying twopointer+sliding window 
+        int left=0;
+        int maxlen=0;
+        int count=0;
+        for( int right=0;right< nums.length;right++){
+            if( nums[right]==0){
+                count++;
             }
-            // ager k 0 n neche chala gaya mena negative m to fir check karnege ki i bhi 0 hia kya ager i 0 hoga to k ko ek bada denge or window ko shringk kar denge i++ kar k
-            while(k<0){
-                if( nums[i]==0){
-                    k++;
+            while(count>k){
+                if(nums[left]==0){
+                   count--; 
                 }
-                i++;
+                left++;
             }
-            // maxlength heck karte jayenge each window k liye privous maclength se or j ko increase karte chalenge
-            maxlength=Math.max(maxlength,j-i+1);
-            j++;    
+            maxlen=Math.max(maxlen,right-left+1);
         }
-        return maxlength;
+        
+        return maxlen;
     }
 }
