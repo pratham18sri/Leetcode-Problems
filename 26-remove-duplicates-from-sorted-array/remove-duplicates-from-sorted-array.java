5f7b67b6b4b24fb2bae8a6e96db80b909ca275a1
PRATHAM SRIVASTAV <139158707+pratham18sri@users.noreply.github.com>
2026-04-06 16:03:31 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (52.12%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,9 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // Set<Integer> set=new HashSet<>(); se ho jaygea or 2 pointer se bhi 
-        int i=0,j=0;
-        while(j<nums.length){
-            if(nums[i]!=nums[j]){
+        // if we done this using SET sapce complexity increase by O(N)
+        // O(N) time complexity
+        int i=0;
+        int j=0;
+        while(j< nums.length){
+            if( nums[i]!=nums[j]){
                 i++;
                 nums[i]=nums[j];
             }
