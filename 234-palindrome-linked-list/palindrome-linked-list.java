5b9d902c8dc92e6a0e1cc8463c47a80744567d62
PRATHAM SRIVASTAV <139158707+pratham18sri@users.noreply.github.com>
2026-04-14 00:18:19 +0530

    Time: 4 ms (69.17%) | Memory: 94.3 MB (71.29%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,50 +9,38 @@
  * }
  */
 class Solution {
+    private ListNode reverse(ListNode head){
+        ListNode prev=null;
+        ListNode current=head;
+        ListNode next=null;
+        while(current!=null){
+            next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+        }
+        return prev;
+    }
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
+        // middle
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-
-        // 1. Find the middle of the linked list using slow and fast pointers.
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // 2. Reverse the second half of the linked list.
-        ListNode secondHalfHead = reverseList(slow);
-
-        // 3. Compare the first half and the reversed second half.
-        ListNode temp1 = head;
-        ListNode temp2 = secondHalfHead;
-
-        while (temp2 != null) { // Important: Iterate until the end of the reversed second half
-            if (temp1.val != temp2.val) {
+        // next half
+        ListNode sechalf=reverse(slow);
+        // check
+        ListNode temp1=head;
+        ListNode temp2=sechalf;
+        while(temp2 != null){
+            if(temp1.val!=temp2.val){
                 return false;
             }
-            temp1 = temp1.next;
-            temp2 = temp2.next;
+            temp1=temp1.next;
+            temp2=temp2.next;
         }
-
         return true;
-    }
-
-    // Helper function to reverse a linked list.
-    private ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode current = head;
-        ListNode next = null;
-
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        return prev; // prev is the new head
     }
 }
