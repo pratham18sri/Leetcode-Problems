90fbdefcf9bfe01290f7e71909c0b548416c6678
PRATHAM SRIVASTAV <139158707+pratham18sri@users.noreply.github.com>
2026-04-22 23:41:31 +0530

    Time: 1 ms (79.29%) | Memory: 42.3 MB (81.16%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,23 +1,26 @@
 class Solution {
-    private int next( int n){
-        int sum=0;
-        while(n>0){
-            int digit=n%10;
-            sum+=digit*digit;
-            n/=10;
+    
+    public int getNext(int n) {
+        int sum = 0;
+        
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n = n / 10;
         }
+        
         return sum;
     }
+
     public boolean isHappy(int n) {
-        // map m check kar lenege ager bo number hog to matlab kbhi 1 qayega he nhi bo repet hota rhega 
-        HashSet<Integer> seen=new HashSet<>();
-        while(n!=1){
-            if(seen.contains(n)){
-                return false;
-            }
-            seen.add(n);
-            n=next(n);
-        }
-        return true;
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = getNext(slow);              // 1 step
+            fast = getNext(getNext(fast));     // 2 steps
+        } while (slow != fast);
+
+        return slow == 1;
     }
 }
