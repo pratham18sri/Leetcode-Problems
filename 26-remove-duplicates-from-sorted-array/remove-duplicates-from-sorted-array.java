9b638f1b08bc24470ca96854c78febc0ee725fa6
PRATHAM SRIVASTAV <139158707+pratham18sri@users.noreply.github.com>
2026-06-02 20:25:28 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (8.25%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,11 +1,9 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // if we done this using SET sapce complexity increase by O(N)
-        // O(N) time complexity
-        int i=0;
-        int j=0;
-        while(j< nums.length){
-            if( nums[i]!=nums[j]){
+        // appply two pointer fro this and solve thjs problem in O(N)
+        int i=0,j=0;
+        while(j<nums.length){
+            if(nums[i]!=nums[j]){
                 i++;
                 nums[i]=nums[j];
             }
