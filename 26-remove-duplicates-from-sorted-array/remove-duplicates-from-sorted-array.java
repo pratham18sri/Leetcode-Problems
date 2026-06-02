65d611ac95ba5599a4eee2aff0b12a1c8dc81c61
PRATHAM SRIVASTAV <139158707+pratham18sri@users.noreply.github.com>
2026-06-02 20:36:39 +0530

    Time: 4 ms (5.57%) | Memory: 46.7 MB (40.82%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,14 +1,26 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         // appply two pointer fro this and solve thjs problem in O(N)
-        int i=0,j=0;
-        while(j<nums.length){
-            if(nums[i]!=nums[j]){
-                i++;
-                nums[i]=nums[j];
-            }
-            j++;
+        // int i=0,j=0;
+        // while(j<nums.length){
+        //     if(nums[i]!=nums[j]){
+        //         i++;
+        //         nums[i]=nums[j];
+        //     }
+        //     j++;
+        // }
+        //  
+        // return i+1;
+        
+        // now solve useing set
+        Set<Integer> set=new LinkedHashSet<>();
+        for( int i: nums){
+            set.add(i);
         }
-        return i+1;
+        int idx=0;
+        for( int i: set){
+            nums[idx++]=i;
+        }
+        return set.size();
     }
 }
