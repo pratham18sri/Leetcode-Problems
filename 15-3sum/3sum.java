5023391aef4c5466dd6ad1624cf000df1a7daed5
PRATHAM SRIVASTAV <139158707+pratham18sri@users.noreply.github.com>
2026-06-16 22:26:46 +0530

    Time: 34 ms (60.32%) | Memory: 59 MB (73.75%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> res=new ArrayList<>();
         for( int i=0;i< nums.length;i++){
-            if( i>0 && (nums[i]==nums[i-1])) continue;
+            if(i>0 && (nums[i]==nums[i-1])) continue;
             int left=i+1;
             int right=nums.length-1;
             while( left< right){
