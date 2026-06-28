92b4deeed4e77663263940339235e6f1c73d490d
PRATHAM SRIVASTAV <139158707+pratham18sri@users.noreply.github.com>
2026-06-28 23:51:59 +0530

    Time: 2 ms (74.38%) | Memory: 45.8 MB (9.17%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -4,9 +4,9 @@ class Solution {
         for( int i: nums){
             set.add(i);
         }
-        for (int i = 1; ; i++) {
-            if (!set.contains(i * k)) {
-                return i * k;
+        for( int i=1; ;i++){
+            if(!set.contains(i*k)){
+                return i*k;
             }
         }
     }
