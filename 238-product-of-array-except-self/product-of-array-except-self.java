7fbe355617cbbef095ecfc851a2496bbcbf1825e
PRATHAM SRIVASTAV <139158707+pratham18sri@users.noreply.github.com>
2026-08-14 21:48:09 +0530

    Time: 2 ms (93.89%) | Memory: 72.3 MB (6.55%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -6,17 +6,17 @@ class Solution {
         for( int i=0;i< nums.length;i++){
             if( nums[i]==0) zcount++;
             else{
-                mul*= nums[i];
+                mul*=nums[i];
             }
         }
         for( int i=0;i< nums.length;i++){
             if(zcount>1) res[i]=0;
             else if(zcount==1){
-                if( nums[i]==0) res[i]= mul;
+                if(nums[i]==0) res[i]= mul;
                 else res[i]=0;
             }
             else{
-                res[i] = mul / nums[i];
+                res[i]= mul/nums[i];
             }
         }
         return res;
